d9d3ac0b399d49cf5174f5ddcda74b6e2ee17210
Jihoon Shin <jihoon.shin.skt@gmail.com>
2016-10-12 22:53:13 +0900

    html template 추가

---
+++ src/main/java/kr/jason/web/MyModel.java
@@ -0,0 +1,13 @@
+package kr.jason.web;
+
+public class MyModel {
+	private String name;
+
+	public MyModel(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
--- src/main/java/kr/jason/web/WelcomeController.java
+++ src/main/java/kr/jason/web/WelcomeController.java
@@ -1,16 +1,30 @@
 package kr.jason.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
+	
 	@GetMapping("/helloworld")
-	public String welcome(String name, int age, Model model){
-		System.out.println("name= "+name + " age= "+age);
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+	public String welcome(Model model){
+	//public String welcome(String name, int age, Model model){
+		//System.out.println("name= "+name + " age= "+age);
+		//model.addAttribute("name", name);
+		//model.addAttribute("age", age);
+		
+		//model.addAttribute("name","Chris");
+		//model.addAttribute("value",10000);
+		//model.addAttribute("taxed_value",30);
+		//model.addAttribute("in_ca",true);	
+		
+		List<MyModel> repo = Arrays.asList(new MyModel("test1"), new MyModel("test2"));
+		model.addAttribute("repo", repo);
+		
 		return "welcome";
 	}
 
