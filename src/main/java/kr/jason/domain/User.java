0ff4e86dcae3dabb8a941e7ee2fafd6699b8f97f
Jihoon Shin <jihoon.shin.skt@gmail.com>
2016-10-16 22:10:30 +0900

    수정하고 db저장까지 구

---
+++ src/main/java/kr/jason/domain/User.java
@@ -0,0 +1,51 @@
+package kr.jason.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable=false, length=20)
+	public String userID;
+	
+	public String password;
+	public String name;
+	public String email;
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userID=" + userID + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+
+	public void update(User newUser) {
+		this.password = newUser.password;
+		this.name = newUser.name;
+		this.email = newUser.email;	
+	}
+	
+
+}
--- src/main/java/kr/jason/web/UserController.java
+++ src/main/java/kr/jason/web/UserController.java
@@ -3,27 +3,64 @@ package kr.jason.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.jason.domain.User;
+import kr.jason.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
+	//private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@GetMapping("/form")
+	public String form(){
+		return "/user/form";
+	}
+	@PostMapping("")
 	public String create(User user, Model model){
 		System.out.println("user= "+user);
-		users.add(user);
-		model.addAttribute("userID", user.userID);
-		model.addAttribute("name", user.name);
-		return "redirect:/list";
+		//users.add(user);
+		userRepository.save(user);
+		//model.addAttribute("userID", user.userID);
+		//model.addAttribute("name", user.name);
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	//@RequestMapping(value="/users", method = RequestMethod.GET)			
+	@GetMapping("")
 	public String list(Model model){
-		model.addAttribute("users", users);
-		return "list";
+		//model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model){
+		System.out.println(id);
+		User user = userRepository.findOne(id);
+		model.addAttribute("user", user);
+		return "/user/updateForm";
+	}
+	
+	//@PostMapping("{id}")
+	@PutMapping("{id}")
+	public String update(@PathVariable Long id, User newUser){
+		User user = userRepository.findOne(id);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
